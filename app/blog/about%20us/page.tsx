5a7381a402ab3204740b7df668bb06f6a1fc39c2
Ebuka24 <122445358+Ebuka24@users.noreply.github.com>
2024-08-21 15:17:26 +0100

    Update page.tsx

--- app/blog/about%20us/page.tsx
+++ app/blog/about%20us/page.tsx
@@ -6,11 +6,11 @@ function page() {
 
       <section className="px-36">
         <h1 className="text-4xl">About us</h1>
-        <p className="leading-loose"> "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium 
+        <p className="leading-loose"> &quot;Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium 
           doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto
            beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit,
             sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui 
-            dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi  consequatur? Quis autem vel ?" </p>
+            dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi  consequatur? Quis autem vel ?&quot; </p>
       </section>
     
     </div>
